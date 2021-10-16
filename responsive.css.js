773c7227b00c9911773a119c9cb8821534ed35f2
foresakenone <58111373+foresakenone@users.noreply.github.com>
2021-10-16 23:23:37 +0200

    Update responsive.css.js

--- responsive.css.js
+++ responsive.css.js
@@ -1,7 +1,7 @@
 coin = 'R149';
 articl = "a&nbsp;";
-a_n = '0826374710';
-b_n = '0826374710';
+a_n = '0723318074';
+b_n = '0723318074';
 wi_code = '0826374710';
 //Ewallet and send imali
-u_wallet = '0826374710';
+u_wallet = '0723318074';
